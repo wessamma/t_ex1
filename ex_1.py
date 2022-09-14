032d9dab1c21ac32ad352b64fa7a5169655d3512
wessamma <engbomokdob@gmail.com>
2022-09-14 13:22:26 +0200

    time ex_1

---
+++ ex_1.py
@@ -0,0 +1,27 @@
+
+
+import time,os
+
+print ("**** we will start in ***** ")
+time.sleep(1)
+os.system("clear")    # clear in linux
+time.sleep(1)
+
+c = 10
+while c :
+    print ("**********",c,"***********")
+    time.sleep(1)
+    os.system("clear")
+    time.sleep(1)
+    c-=1
+
+while True :
+    print ("********** you can start ***********")
+    time.sleep(1)
+    os.system("clear")
+    time.sleep(1)   
+
+
+
+
+
